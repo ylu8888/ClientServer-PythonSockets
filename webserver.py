d651b4b63d8240051fce10c211bbb635513b9e3b
ylu <123523291+ylu8888@users.noreply.github.com>
2024-02-26 16:37:32 -0500

    fin part 1

--- webserver.py
+++ webserver.py
@@ -1,8 +1,28 @@
-"""In this part of the assignment, you will learn the basics of socket programming for TCP
-connections in Python: how to create a socket, bind it to a specific address and port, as well as
-send and receive a HTTP packet. You will also learn some basics of HTTP header format.
-Develop a web server that handles one HTTP request at a time. Your web server should be able
-to (a) accept and parse the HTTP request, get the requested file from the server’s file system, (b)
-create an HTTP response message consisting of the requested file preceded by header lines, and
-then (c) send the response directly to the client. (d) If the requested file is not present in the
-server, the server should send an HTTP “404 Not Found” message back to the client."""
+#Yang Lu 114607667 CSE 310 Assignment 1 Part 1
+from socket import *
+
+host = '127.0.0.1'  #localhost
+serverPort = 8000
+
+serverSocket = socket(AF_INET,SOCK_STREAM) #creating the socket
+serverSocket.bind((host, serverPort))
+
+serverSocket.listen(1) #listen for connections
+print('The server is ready to receive')
+
+while True:
+    connectionSocket, addr = serverSocket.accept() # accept incoming connections
+    request = connectionSocket.recv(1024).decode() #receives the http request
+    
+    htmlfile = request.split()[1][1:] #this extracts out the name of hello.html
+
+    try:
+        with open(htmlfile, 'rb') as hello: #opening the html file
+            responseData = hello.read() #reading the html content
+            response = "HTTP/1.1 200 OK\r\n\r\n".encode() + responseData
+
+    except:  #if theres no html file found
+        response = "HTTP/1.1 404 Not Found\r\n\r\nError 404 Page Not Found".encode() 
+    
+    connectionSocket.sendall(response) #send response back
+    connectionSocket.close()
